34b09b2bbbd004a3549f5cee55477e5a58d69016
Aaron Zuspan <aa.zuspan@gmail.com>
2024-07-28 16:07:35 -0700

    Boilerplate

---
+++ src/spinasm_lsp/main.py
@@ -0,0 +1,11 @@
+def my_function() -> int:
+    """
+    This is a placeholder function.
+
+    Returns
+    -------
+    int
+        The number 42.
+
+    """
+    return 42
---
+++ tests/test_template.py
@@ -0,0 +1,5 @@
+from spinasm_lsp.main import my_function
+
+
+def test_my_function():
+    assert my_function() == 42
